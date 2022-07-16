a3f643f8792068164e17731974d1abbd68997e96
tirexias <ignacio.munozullrich@gmail.com>
2022-07-16 11:55:56 -0300

    test

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "hola";
 const nuevoNum = 12;
 
 // Crea una variable booleana:
-const nuevoBool = True;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
