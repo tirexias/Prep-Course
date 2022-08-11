5fdec6eea603cd5a80cafb8c3abc6bb97615e976
tirexias <ignacio.munozullrich@gmail.com>
2022-08-11 19:42:36 -0300

    second commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -7,15 +7,20 @@ function obtenerMayor(x, y) {
   // Tu código:
   if (x >= y) {
     return x;
-  } else {
+  } else if (x < y) {
     return y;
-  } 
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18) {
+    return "Allowed";
+  } else {
+    return "Not allowed";
+  }
 }
   
 function conection(status) {
