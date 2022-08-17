eda07dabb6fb488b3aa1d0c3ad74e96352f88663
tirexias <ignacio.munozullrich@gmail.com>
2022-08-16 23:41:11 -0300

    last one, probably

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -149,6 +149,19 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 === 0 || num2 === 0 || num3 ===0) {
+    return "Error";
+  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+  return "Hay negativos";
+  } else if (num3 > num1 && num3 > num2) {
+    num3++
+    return num3;
+  } else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return "Número 1 es mayor y positivo";
+  } else {
+    return false;
+  }
+  
 }
 
 function esPrimo(numero) {
@@ -157,20 +170,36 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2) {
+  return false;
+  } else {
+    for (let i = 2; i < numero; i++) {
+     if (numero % i === 0) {
+      return false;
+     } 
+    }
+    return true;
+  }
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+if (valor === true) {
+  return "Soy verdadero";
+} else {
+  return "Soy falso";
+}
 }
 
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  for (var i = 0; i < 61; i++) {
+    return 
+  }
 }
 
 function tieneTresDigitos(numero){
