da1ce2524ecf253ae371e467bf1379c6841db67f
tirexias <ignacio.munozullrich@gmail.com>
2022-09-08 17:48:37 -0300

    done

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -84,6 +84,9 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
 function agregarMetodo() {
   //La función agrega un método "datos" a la clase Persona que toma el nombre y la edad de la persona y devuelve: 
   //Ej: "Juan, 22 años"
+  Persona.prototype.datos = function() {
+    return this.nombre + ', ' + this.edad + ' años';
+  }
 }
   
 
